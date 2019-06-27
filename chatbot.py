8aa7680178836f5de00f1546fc422d3cbd5809cf
kimb4 <52220800+kimb4@users.noreply.github.com>
2019-06-27 15:01:32 -0700

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,33 @@
+# --- Define your functions below! ---
+def say_hello(name):
+    print ("Hello "+name)
+
+
+# --- Put your main program below! ---
+def main():
+    user_name=input("whats your name? ")
+    say_hello(user_name)
+    answer=input("How is your day? " )
+    story()
+    friend()
+    joke()
+    jokea()
+    lastr()
+    answer=input()
+def story():
+    answer=input("If it's okay I'm going to tell you a story about one of my friends Anh to make your day even better " )
+def friend():
+    answer=input("Anh jumped into the pond where ducks feasted on a piece of bread. They sang a satanic chat that scared Beyonce and Rihanna. They danced and sacrificed their leader who tried to eat the ducks. They tried and failed. Now its time for a joke")
+
+def joke():
+    answer=input("Why did the chicken cross the road")
+def jokea():
+    answer=input("To get to the other side. hahahahaha. Alright last one friend")
+
+def lastr():
+    answer=input("Math the only place where people buy 64 watermelons and nobody wonders why. Taaaa daaaa. Thanks for chatting with me friend")
+# print("Well I'm going to tell you all about it!")
+
+# DON'T TOUCH! Setup code that runs your main() function.
+if __name__ == "__main__":
+  main()
